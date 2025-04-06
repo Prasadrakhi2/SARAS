158603f1402810a64b1bb3c494ff5a3ee0044ce6
rakhi <prasadrakhi20004@gmail.com>
2025-04-06 12:10:03 +0530

    profile section

--- Frontend/SARAS/src/App.jsx
+++ Frontend/SARAS/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route path="/" element={<UserLayout />}>
           {/*User Layout */}
           <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
         <Route>{/*Admin Layout */}</Route>
       </Routes>
